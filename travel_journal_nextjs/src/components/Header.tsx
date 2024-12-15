c70df454a5236fdd75165fa8680c7e9815bd256d
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-12-15 16:34:30 +0100

    Inter font added

--- travel_journal_nextjs/src/components/Header.tsx
+++ travel_journal_nextjs/src/components/Header.tsx
@@ -3,8 +3,8 @@ import { IoEarth } from 'react-icons/io5'
 
 const Header = () => {
   return (
-    <div className='flex flex-row bg-rose-500 text-center items-center justify-center p-4 gap-2'>
-        <div className='text-3xl text-white'>
+    <div className='flex flex-row bg-[#F55A5A] items-center justify-center p-4 gap-2'>
+        <div className='text-2xl text-white'>
         <IoEarth />
         </div>
         <div className='text-white font-medium'>
