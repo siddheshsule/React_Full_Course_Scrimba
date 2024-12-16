00c95ce74d76c5fcd28d2e2173ed7acb08b5ec56
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-12-16 16:26:26 +0100

    Header added

--- chef_claude_nextjs/src/components/Header.tsx
+++ chef_claude_nextjs/src/components/Header.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-        <div className='flex flex-row gap-3 items-center justify-center p-5 bg-gray-100 shadow-md'>
-            <img src="/chef-claude-icon.png" alt="chef claude icon" className='w-[60px] h-auto'/>
-            <h1 className='text-4xl font-bold'>Chef Claude</h1>
-        </div>
-
+      <div className="flex flex-row gap-3 items-center justify-center p-5 bg-gray-100 shadow-md">
+        <img
+          src="/chef-claude-icon.png"
+          alt="chef claude icon"
+          className="w-[50px] h-auto"
+        />
+        <h1 className="text-3xl font-medium">Chef Claude</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
