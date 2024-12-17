d6d8c8cdf7bfbeac8036bfaed7004c01e8456947
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-12-17 13:14:04 +0100

    handleSubmit function updated

--- chef_claude_nextjs/src/components/Main.tsx
+++ chef_claude_nextjs/src/components/Main.tsx
@@ -1,31 +1,50 @@
 'use client'
 
-const Main = () => {
-  const ingredients = [
-    "Chicken breasts",
-    "Beef",
-    "Pork",
-    "Fish",
-    "Vegetables",
-    "Fruits",
-    "Spices",
-    "Herbs",
-  ];
+import { FormEvent } from "react";
+import { useState } from "react";
 
-  const handleButtonClick = () => {
-    console.log("Button clicked");
+
+const Main = () => {
+
+  const[ingredients, setIngredients] = useState<string[]>([]);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget as HTMLFormElement);
+    const newIngredient = formData.get("ingredient") as string;
+    if(newIngredient === "") {
+      alert("Please enter an ingredient.");
+      return;
+    }
+    if(!ingredients.includes(newIngredient)) {
+      setIngredients([...ingredients, newIngredient]);
+
+    } else {
+      alert("This ingredient is already on the list.");
+      return;
+    }
+    console.log(ingredients);
+  }
+
+  const handleReset = () => {
+    setIngredients([]); 
+
   }
   return (
     <div className="mt-16 flex flex-col items-center align-center p-5">
       <div className="flex flex-col">
-        <form action="submit" className="flex flex-row gap-3">
+        <form className="flex flex-row gap-3" onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="e.g. oregano"
             className="border p-2 rounded-lg min-w-[400px]"
+            name="ingredient"
           />
-          <button onClick={handleButtonClick} className="bg-black text-blue-50 py-2 text-lg px-8 rounded-lg">
+          <button className="bg-black text-blue-50 py-2 text-lg px-8 rounded-lg">
             + Add ingredient
+          </button>
+          <button onClick={handleReset} className="bg-white text-black border-2 hover:bg-gray-200 transition-all duration-500 py-2 text-lg px-8 rounded-lg">
+            Reset
           </button>
         </form>
 
