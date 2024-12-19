d691bb0e51c7e26c01fc8ec8060240073d604e7b
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-12-19 09:08:36 +0100

    Conditional rendering added

--- chef_claude_nextjs/src/components/Main.tsx
+++ chef_claude_nextjs/src/components/Main.tsx
@@ -2,6 +2,7 @@
 
 import { FormEvent } from "react";
 import { useState } from "react";
+import CallToAction from "./CallToAction";
 
 
 const Main = () => {
@@ -38,15 +39,15 @@ const Main = () => {
             className="border p-2 rounded-lg min-w-[400px]"
             name="ingredient"
           />
-          <button className="bg-black text-blue-50 py-2 text-lg px-8 rounded-lg">
+          <button className="bg-black hover:bg-gray-700 text-blue-50 py-2 px-8 rounded-lg">
             + Add ingredient
           </button>
-          <button onClick={handleReset} className="bg-white text-black border-2 hover:bg-gray-200 transition-all duration-500 py-2 text-lg px-8 rounded-lg">
+          <button onClick={handleReset} className="bg-white text-black border-2 hover:bg-gray-200 transition-all duration-500 py-2 px-8 rounded-lg">
             Reset
           </button>
         </form>
 
-        <div className="ml-5">
+        {ingredients.length > 0 ? <section className="m-5">
           <h2 className="text-3xl font-semibold mt-5">Ingredients on hand:</h2>
           <ul className="space-y-3 text-lg ml-5 mt-5">
             {ingredients.map((item, index) => (
@@ -55,7 +56,10 @@ const Main = () => {
               </li>
             ))}
           </ul>
-        </div>
+          {ingredients.length > 2 ? <div className="mt-12">
+            <CallToAction />
+          </div> : null}            
+        </section> : null}
       </div>
     </div>
   );
