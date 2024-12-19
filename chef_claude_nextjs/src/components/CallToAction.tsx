feda5393974dbfaed6c23129b0d9be59e5569c89
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-12-19 09:08:25 +0100

    Call to action button added

---
+++ chef_claude_nextjs/src/components/CallToAction.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CallToAction = () => {
+  return (
+    <div className="flex flex-row items-center justify-center mx-8  bg-gray-200 p-5 rounded-lg space-x-10">
+      <div className="flex flex-col space-y-4">
+        <h2 className="font-medium text-xl">Ready for a receipe?</h2>
+        <p className="text-gray-400">Generate a receipe from your list of ingredients.</p>
+      </div>
+      <div>
+        <button className="bg-amber-600 hover:bg-amber-500 text-blue-50 py-2 px-8 rounded-lg">
+          Get a receipe
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
