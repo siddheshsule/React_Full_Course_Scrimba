784ccdfbe0b8ea47529e442f1823b55a4c35afef
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-12-20 16:42:04 +0100

    Meme generator project completed

--- meme-generator-nextjs/src/components/Main.tsx
+++ meme-generator-nextjs/src/components/Main.tsx
@@ -1,18 +1,45 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+interface Meme {
+  topText: string;
+  bottomText: string;
+  url: string;
+}
 
 const Main = () => {
-  const [meme, setMeme] = useState({
+  const [meme, setMeme] = useState<Meme>({
     topText: "One does not simply",
     bottomText: "Walk into Mordor",
-    imgUrl: "http://i.imgflip.com/1bij.jpg",
+    url: "http://i.imgflip.com/1bij.jpg",
   });
+
+  const [allMemes, setAllMemes] = useState<Meme[]>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setMeme((prevMeme) => ({ ...prevMeme, [name]: value }));
   };
+
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * allMemes.length);
+    const randomMeme = allMemes[randomIndex];
+    console.log(randomMeme);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      url: randomMeme.url,
+    }));
+  };
+
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(data => {setAllMemes(data.data.memes);
+    // console.log(allMemes);
+    });
+  }, []);
 
   return (
     <main className="flex flex-col align-middle justify-center items-center p-5">
@@ -38,12 +65,12 @@ const Main = () => {
             onChange={handleChange}
           />
         </label>
-        <button className="col-span-2 my-6 bg-purple-500 py-2 px-3 text-white font-semibold rounded-md ">
+        <button onClick={handleClick} className="col-span-2 my-6 bg-purple-500 py-2 px-3 text-white font-semibold rounded-md ">
           Get a new meme image
         </button>
       </div>
       <div className="relative flex flex-col justify-center items-center">
-        <img src="http://i.imgflip.com/1bij.jpg" />
+        <img src={meme.url} />
         <span className="absolute top-0 text-center my-4 px-1 font-impact text-4xl uppercase text-white tracking-wide [text-shadow:2px_2px_0_#000,-2px_-2px_0_#000,2px_-2px_0_#000,-2px_2px_0_#000,0_2px_0_#000,2px_0_0_#000,0_-2px_0_#000,-2px_0_0_#000]">
           {meme.topText}
         </span>
